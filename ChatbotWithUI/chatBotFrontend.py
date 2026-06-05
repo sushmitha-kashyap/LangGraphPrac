d8868fd8394ee30468f0183870f507983abe687c
sushmitha <kashyap.sushmitha01@gmail.com>
2026-06-05 13:55:08 +0530

    chatbot with ui files

---
+++ ChatbotWithUI/chatBotFrontend.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from chatBotBackend import chatbot
+from langchain_core.messages import HumanMessage
+
+CONFIG = {'configurable': {'thread_id': 'randomID'}}
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+    #add user input to message history
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config=CONFIG)
+    ai_response = response['messages'][-1].content
+
+    st.session_state['message_history'].append({'role':'assistant','content':ai_response})
+    with st.chat_message('assistant'):
+        st.text(ai_response)
